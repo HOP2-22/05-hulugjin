4b4524b8061f39e631e77f9a07c5376540ad94b7
22-hop2- <22-hop2-@orgils-iMac.local>
2022-10-24 19:31:28 +0800

    intilaze

---
+++ src/pages/Team.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export const Team = () => {
+  const { te } = useParams();
+
+  //console.log(team);
+
+  return <div>team:</div>;
+};
---
+++ src/pages/Users.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export const UserId = () => {
+  /*
+  const { userId } = useParams();
+  const user = users.find((user) => {
+    return user.username === userId;
+  });
+
+  const style = {
+    box: {
+      width: "100vw",
+      height: "100vh",
+      backgroundColor: "white ",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    ins: {
+      display: "flex",
+      flexDirection: "column",
+      width: "360px",
+      height: "370px",
+      backgroundColor: "red",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  };
+  return (
+    <div style={style.box}>
+      <div style={style.ins}>
+        <img src={user.avatar} width={100} height={100} alt={user.username} />
+        <p>UserName: {user.username}</p>
+        <p>Email: {user.email}</p>
+        <p>Birth: {user.date_of_birth}</p>
+      </div>
+    </div>
+  );
+  */
+};

af7cdf2d7aaa594c06b5d82295b411e543b27254
22-hop2- <reflix.b@gmail.com>
2022-11-30 19:45:01 +0800

    game

--- block2/game/src/App.js
+++ block2/game/src/App.js
@@ -1,14 +1,36 @@
 import "./App.css";
 import React from "react";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
+import { useEffect, useState } from "react";
 import Header from "./Components/Header";
+import Hammer from "./Img/hammer.png";
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  document.addEventListener("mousemove", onMouseUpdate, false);
+  function onMouseUpdate(e) {
+    setMousePosition({
+      x: e.pageX,
+      y: e.pageY,
+    });
+  }
+  useEffect(() => {
+    return () => document.removeEventListener("mousemove", onMouseUpdate, true);
+  }, [onMouseUpdate]);
+
   return (
     <div>
+      <img
+        src={Hammer}
+        style={{
+          position: "absolute",
+          width: "50px",
+          height: "50px",
+          zIndex: "4",
+          left: `${mousePosition.x}px`,
+          top: `${mousePosition.y}px`,
+        }}
+        alt=""
+      />
       <Header />
     </div>
   );
--- block2/game/src/Components/Header.jsx
+++ block2/game/src/Components/Header.jsx
@@ -1,126 +1,181 @@
-import React, { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import { styled } from "@mui/material/styles";
-import Upper from "../Img/UpperBack.png";
-import Lower from "../Img/LowerBack.png";
-import Green from "../Img/body.png";
-import { Button } from "@mui/material";
-import { style } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import upperback from "../Img/UpperBack.png";
+import back from "../Img/LowerBack.png";
+import hun from "../Img/body.png";
+import { Container } from "@mui/system";
+import { Alert } from "@mui/material";
+import Hammer from "../Img/hammer.png";
 
-const Rat = ({ active }) => {
+function Header({ active }) {
+  const [click, setClick] = useState(true);
+  const [seconds, setSeconds] = useState(30);
   const [score, setScore] = useState(0);
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        margin: "0",
-        position: "relative",
-        height: "256px",
-        width: "128px",
-      }}
-    >
-      <div>dsfsd</div>
-      <img
-        src={Upper}
-        style={{
-          position: "absolute",
-          top: 0,
-          width: "128px",
-          height: "128px",
-          zIndex: 1,
-        }}
-        alt=""
-      />
-      <img
-        onClick={() => {
-          active = false;
-          setScore((e) => e + 1);
-          console.log(score);
-        }}
-        src={Green}
-        style={{
-          width: "64px",
-          marginLeft: "10px",
-          transform: `translateY(${active ? "0px" : "-80px"})`,
-          transition: "all 1s",
-          position: "absolute",
-          left: "24px",
-          bottom: 0,
-          zIndex: 2,
-        }}
-        alt=""
-      />
-      <img
-        src={Lower}
-        style={{
-          position: "absolute",
-          width: "128px",
-          height: "144px",
-          bottom: 0,
-          zIndex: 3,
-        }}
-        alt=""
-      />
-    </div>
-  );
-};
-
-export const Header = () => {
-  const [rats, setRats] = useState(
-    new Array(3).fill(null).map(() => new Array(5).fill(false))
-  );
+  const [min, setMin] = useState(0);
+  const [start, setStart] = useState(false);
+  const [col, setCol] = useState(new Array(15).fill(false));
+  const [on, setOn] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [valueX, setValueX] = useState();
+  const [valueY, setValueY] = useState();
+  const [mouseClick, setMouseClick] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      let newRats = new Array(3).fill(null).map(() => new Array(5).fill(false));
-      for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 5; j++) {
-          newRats[i][j] = Math.random() >= 0.5;
-        }
+    let inter = null;
+    inter = setInterval(() => {
+      const translation = () => {
+        let random = Math.floor(Math.random() * 15);
+        col[random] = true;
+        setTimeout(() => {
+          col[random] = false;
+        }, 1000);
+      };
+      if (click) translation();
+    }, [200]);
+    return () => clearInterval(inter);
+  }, [click, col]);
+
+  useEffect(() => {
+    if (start) {
+      const interval = setInterval(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+      if (seconds >= 100) {
+        setMin(min - 1);
+        setSeconds(0);
       }
-      setRats(newRats);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+      if (seconds === 0) {
+        setStart(false);
+        alert(score + " score");
+      }
+
+      return () => clearInterval(interval);
+    }
+  }, [seconds, start]);
 
   return (
-    <>
-      <div></div>
+    <body
+      style={{
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      {" "}
       <div
+        className=""
         style={{
           display: "flex",
+          gap: "100px",
+          width: "100vw",
           justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-
-          flexDirection: "column",
         }}
       >
-        {rats.map((row, index) => (
-          <div
-            key={index}
-            style={{
-              flexDirection: "row",
-              display: "flex",
-            }}
-          >
-            {row.map((rat, index) => (
-              <Rat active={rat} key={index} />
-            ))}
-          </div>
-        ))}
+        <div className="">
+          <p>time:{seconds}</p>
+        </div>
+        <div className="">
+          <p>Score:{score}</p>
+        </div>
       </div>
-    </>
+      <div className=""></div>
+      <div
+        className=""
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          gridTemplateColumns: "Repeat(5,minmax(0 , 1fr))",
+          gap: "20px",
+        }}
+      >
+        {col.map((coloo, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                display: "grid",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img
+                src={upperback}
+                style={{ width: "128px", height: "128px", zIndex: "1" }}
+                alt=""
+              />
+              {start ? (
+                <img
+                  src={hun}
+                  style={{
+                    width: "64px",
+                    height: "128px",
+                    zIndex: "2",
+                    marginTop: " -30px ",
+                    marginLeft: " 30px",
+                    transform: `translateY(${coloo ? "-80px" : "0px"})`,
+                    transition: "all 1s",
+                    pointerEvents: isDisabled ? "none" : "auto",
+                  }}
+                  onClick={() => {
+                    setScore((e) => e + 1);
+                    setIsDisabled(true);
+                    setTimeout(() => {
+                      setIsDisabled(false);
+                    }, 1000);
+                  }}
+                  alt=""
+                />
+              ) : (
+                <img
+                  src={hun}
+                  style={{
+                    width: "64px",
+                    height: "128px",
+                    zIndex: "2",
+                    marginTop: " -30px ",
+                    marginLeft: " 30px",
+                    transform: `translateY(${coloo ? "-80px" : "0px"})`,
+                    transition: "all 1s",
+                  }}
+                  alt=""
+                />
+              )}
+
+              <img
+                src={back}
+                style={{
+                  width: "128px",
+                  height: "124px",
+                  zIndex: "3",
+                  marginTop: " -115px",
+                }}
+                alt=""
+              />
+            </div>
+          );
+        })}
+      </div>
+      <img
+        src={Hammer}
+        style={{
+          position: "absolute",
+          width: "50px",
+          height: "50px",
+          zIndex: "4",
+          left: valueX,
+          top: valueY,
+        }}
+        alt=""
+      />
+      <button
+        onClick={() => {
+          setStart(true);
+          setClick(true);
+        }}
+      >
+        Start
+      </button>
+    </body>
   );
-};
-
+}
 export default Header;
-
-/*
- active = false;
-          transform: `translateY(${active ? "-80px" : "0px"})`;   
-*/
